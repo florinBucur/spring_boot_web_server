c2a70747a70b7e7520e508677ac92b4ba4e0844a
AdryanIoan <adrian.ioan.coman@gmail.com>
2017-05-09 23:10:13 +0300

    userEntity

---
+++ src/main/java/com/innovation/evconnect/entities/Users.java
@@ -0,0 +1,77 @@
+package com.innovation.evconnect.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class Users implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="user_id")
+	private long userID;
+	
+	@Column(name="useranme")
+	private String username;
+	
+	@Column(name="password")
+	private String password;
+	
+	@Column(name="user_role")
+	private String userRole;
+
+	public long getUserID() {
+		return userID;
+	}
+
+	public void setUserID(long userID) {
+		this.userID = userID;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUserRole() {
+		return userRole;
+	}
+
+	public void setUserRole(String userRole) {
+		this.userRole = userRole;
+	}
+
+	public Users(long userID, String username, String password, String userRole) {
+		super();
+		this.userID = userID;
+		this.username = username;
+		this.password = password;
+		this.userRole = userRole;
+	}
+
+	@Override
+	public String toString() {
+		return "Users [userID=" + userID + ", username=" + username + ", password=" + password + ", userRole="
+				+ userRole + "]";
+	}
+}
